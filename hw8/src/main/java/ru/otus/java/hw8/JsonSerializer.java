1efd54bc305fc7a57fe0be0a71c3ae28684a174f
Kseniia Vasileva <ksevasilyeva@gmail.com>
2018-02-04 15:29:42 +0100

    fix - primitive objects serialization, add more tests

--- hw8/src/main/java/ru/otus/java/hw8/JsonSerializer.java
+++ hw8/src/main/java/ru/otus/java/hw8/JsonSerializer.java
@@ -9,15 +9,41 @@ public class JsonSerializer {
     public String toJson(Object object) {
 
         if (object == null) {
-            return "{}";
+            return "null";
         }
 
+        if (object instanceof String) {
+            return String.format("\"%s\"", object);
+        }
+
+        if (object instanceof Number ||
+            object instanceof Boolean) {
+            return object.toString();
+        }
+
+        if (object.getClass().isArray()) {
+            return appendArray(object);
+        }
+
+        if (object instanceof Collection) {
+            return appendCollection(object);
+        }
+
+        try {
+            return appendObject(object);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed serializing", e);
+        }
+    }
+
+    public String appendObject(Object object) {
         StringBuilder resultString = new StringBuilder("{");
 
         String elemSeparator = "";
         for (Field field : object.getClass().getDeclaredFields()) {
             Object fieldValue;
             try {
+                field.setAccessible(true);
                 fieldValue = field.get(object);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
@@ -28,49 +54,32 @@ public class JsonSerializer {
             }
             resultString.append(elemSeparator)
                 .append(String.format("\"%s\":", field.getName()))
-                .append(appendValue(fieldValue));
+                .append(toJson(fieldValue));
             elemSeparator = ",";
         }
 
         return resultString.append('}').toString();
     }
 
-    public String appendValue(Object fieldValue) {
-
-        if (fieldValue instanceof Number ||
-            fieldValue instanceof Boolean) {
-            return fieldValue.toString();
-        }
-
-        if (fieldValue instanceof String) {
-            return String.format("\"%s\"", fieldValue);
-        }
-
-        if (fieldValue.getClass().isArray()) {
-            StringBuilder resultString = new StringBuilder("[");
-            for (int i = 0; i < Array.getLength(fieldValue); i++) {
-                resultString.append(appendValue(Array.get(fieldValue, i)));
-                if (i != Array.getLength(fieldValue) - 1) {
-                    resultString.append(",");
-                }
+    public String appendArray(Object fieldValue) {
+        StringBuilder resultString = new StringBuilder("[");
+        for (int i = 0; i < Array.getLength(fieldValue); i++) {
+            resultString.append(toJson(Array.get(fieldValue, i)));
+            if (i != Array.getLength(fieldValue) - 1) {
+                resultString.append(",");
             }
-            return resultString.append("]").toString();
         }
+        return resultString.append("]").toString();
+    }
 
-        if (fieldValue instanceof Collection) {
-            StringBuilder sb = new StringBuilder("[");
-            String elemSeparator = "";
-            for (Object value : (Collection) fieldValue) {
-                sb.append(elemSeparator)
-                    .append(appendValue(value));
-                elemSeparator = ",";
-            }
-            return sb.append("]").toString();
+    public String appendCollection(Object fieldValue) {
+        StringBuilder sb = new StringBuilder("[");
+        String elemSeparator = "";
+        for (Object value : (Collection) fieldValue) {
+            sb.append(elemSeparator)
+                .append(toJson(value));
+            elemSeparator = ",";
         }
-
-        if (fieldValue instanceof Object) {
-            return toJson(fieldValue);
-        }
-        return "{}";
+        return sb.append("]").toString();
     }
 }
--- hw8/src/test/java/ru/otus/java/hw8/test/JsonSerializerTest.java
+++ hw8/src/test/java/ru/otus/java/hw8/test/JsonSerializerTest.java
@@ -1,13 +1,18 @@
 package ru.otus.java.hw8.test;
 
 import com.google.gson.Gson;
+import org.junit.Assert;
 import org.junit.Test;
 import ru.otus.java.hw8.JsonSerializer;
 import ru.otus.java.hw8.data.ClassWithDataFields;
 import ru.otus.java.hw8.data.ClassWithObjectField;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
+import static org.hamcrest.core.Is.is;
 
 public class JsonSerializerTest {
 
@@ -32,5 +37,45 @@ public class JsonSerializerTest {
 
         assertThat("Should serialize object with other class in field", objectToSerialize , samePropertyValuesAs(objectFromJson));
 
+    }
+
+    @Test
+    public void shouldSerializeNullValue() {
+        String targetObject = null;
+
+        String myJson = new JsonSerializer().toJson(targetObject);
+        String gson = new Gson().toJson(targetObject);
+
+        Assert.assertEquals(gson, myJson);
+    }
+
+    @Test
+    public void shouldSerializeStringValue() {
+        String targetObject = "abc";
+
+        String myJson = new JsonSerializer().toJson(targetObject);
+        String gson = new Gson().toJson(targetObject);
+
+        assertThat(gson, is(myJson));
+    }
+
+    @Test
+    public void shouldSerializeIntValue() {
+        int targetObject = 123;
+
+        String myJson = new JsonSerializer().toJson(targetObject);
+        String gson = new Gson().toJson(targetObject);
+
+        assertThat(gson, is(myJson));
+    }
+
+    @Test
+    public void shouldSerializeList() {
+        List<String> targetObject = Arrays.asList("one", "two", "three");
+
+        String myJson = new JsonSerializer().toJson(targetObject);
+        String gson = new Gson().toJson(targetObject);
+
+        assertThat(gson, is(myJson));
     }
 }
